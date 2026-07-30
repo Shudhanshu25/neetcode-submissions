9e8cd527db226cb1b236fc5ebb6f0802622d101a
Shudhanshu Bharat tripathi <161917934+Shudhanshu25@users.noreply.github.com>
2026-07-30 08:55:15 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq1 = {}
+        freq2 = {}
+
+        for ch in s1:
+            freq1[ch] = freq1.get(ch , 0) + 1       #counting frequency
+
+        left = 0
+
+        for right in range(len(s2)):
+            freq2[s2[right]] = freq2.get(s2[right] , 0) + 1         #adding character
+            
+            if right - left + 1 > len(s1):
+                freq2[s2[left]] -= 1
+                if freq2[s2[left]] == 0:
+                    del freq2[s2[left]]
+                left += 1
+            
+            if right - left + 1 == len(s1):
+                if freq1 == freq2:
+                    return True
+        return False
+        
